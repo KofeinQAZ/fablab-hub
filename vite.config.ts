de245ecaa690c09248ea72df0daab519dec62e39
Rasul Kapash <kapasrasul@gmail.com>
2026-05-28 22:45:47 +0500

    fix: absolute final nitro architecture for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import viteReact from '@vitejs/plugin-react'
+import { nitro } from 'nitro/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
-    TanStackRouterVite(),
-    react(),
-    tsconfigPaths()
+    tanstackStart(),
+    nitro({
+      preset: 'vercel'
+    }),
+    viteReact(),
+    tsconfigPaths(),
+    tailwindcss()
   ]
 })
