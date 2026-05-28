1f6c99a73e16c0a06ade41592a535affd3427d12
Rasul Kapash <kapasrasul@gmail.com>
2026-05-28 21:00:52 +0500

    fix: use standard tanstack config for vercel

--- app.config.ts
+++ app.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from '@tanstack/react-start/config'
 
 export default defineConfig({
-  tanstackStart: {
-    server: {
-      preset: "vercel", // Просто пишем строкой, без импортов
-      entry: "src/server",
-    },
+  server: {
+    preset: 'vercel',
   },
-});
+})
